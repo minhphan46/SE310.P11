4e7104c0b7c4e2385c2cade1343c68eb9b32e9aa
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-10-08 15:51:52 +0700

    update

---
+++ Bai4/ProductManagementMVC/Controllers/CatalogController.cs
@@ -0,0 +1,19 @@
+﻿using ProductManagementMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ProductManagementMVC.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            var context = new QuanLySanPhamConnectionString();
+            List<Catalog> dsCatalog = context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+
+    }
+}
---
+++ Bai4/ProductManagementMVC/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using ProductManagementMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ProductManagementMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var context = new QuanLySanPhamConnectionString();
+            List<Product> dsProducts = context.Products.ToList();
+            return View(dsProducts);
+        }
+    }
+}
